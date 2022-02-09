f5720041182d6bf318961f321c298920cf945491
golveramtz <golveramtz@aim.com>
2022-02-08 19:58:02 -0600

    My first Commit

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
+import Coinitem from "./components/Coinitem";
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadData = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    setCoins(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#141414" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Crypto Price Tracker</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a coin..."
+          placeholderTextColor="#858585"
+          onChangeText={(text) => setSearch(text)}
+        />
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={
+          coins.filter(coin => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))
+          }
+        renderItem={({ item }) => {
+          return <Coinitem coin={item} />;
+        }}
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          setRefreshing(true);
+          await loadData();
+          setRefreshing(false);
+          }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#141414",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: "#fff",
+    marginTop: 32,
+    fontSize: 24,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    marginBottom: 10,
+  },
+  list: {
+    width: "90%",
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#fff",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
+    fontSize: 20,
+    padding: 0,
+    marginTop: 28,
+    marginBottom: 4,
   },
 });
+
+export default App;
